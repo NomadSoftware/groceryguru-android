cc418dbd037bbb3a18bd0a3f38e27c86439a2296
Eric Fenderbosch <eric.fenderbosch@segmint.com>
2014-06-15 10:37:09 -0400

    renamed to groceryguru

--- src/biz/nomadsoftware/groceryguru/MainActivity.java
+++ src/biz/nomadsoftware/groceryguru/MainActivity.java
@@ -1,26 +1,26 @@
-package biz.nomadsoftware.market_minder;
+package biz.nomadsoftware.groceryguru;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
+import biz.nomadsoftware.market_minder.R;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBarActivity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.os.Build;
 
 public class MainActivity extends ActionBarActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		// Settings.Secure.ANDROID_ID
 		setContentView(R.layout.activity_main);
 
 		if (savedInstanceState == null) {
 			getSupportFragmentManager().beginTransaction()
-					.add(R.id.container, new PlaceholderFragment()).commit();
+			.add(R.id.container, new PlaceholderFragment()).commit();
 		}
 	}
 
